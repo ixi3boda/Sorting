33743c66787cd94cb5b7922f435f9980cceea4db
ixi3boda <abdelrahmanessm508@gmail.com>
2025-02-28 01:01:11 +0200

    final code

--- main.cpp
+++ main.cpp
@@ -1,19 +1,107 @@
 #include <iostream>
-
+#include <vector>
+using namespace std;
 // TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+void bubbleSort(vector <int> arr, int size) {
+    bool isSorted;
+    for (int i = 0; i < size - 1; i++) {
+        isSorted = true;
+        for (int j = 0; j < size - 1 - i; j++) {
+            // if value before is greater than after --> swap
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                isSorted = false;
+            }
+        }
+        if (isSorted) break;
+    }
+}
+
+
+
+void insertionSort(vector <int> arr, int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void selectionSort(vector <int> arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        swap(arr[i], arr[min]);
+    }
+}
+
+void printArray(vector <int> arr, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+}
+
+vector <int> generateRandArray(int size) {
+    vector <int> arr(size);
+    for (int i = 0; i < size; i++)
+        arr[i] = rand();
+
+    return arr;
+}
+
+void measureRuntime(int size) {
+    clock_t start, end;
+    double cpu_time_used;
+
+    vector <int> arr = generateRandArray(size);
+
+    // Bubble Sort
+    start = clock();
+    bubbleSort(arr, size);
+    end = clock();
+    cpu_time_used = (((double) (end - start)) / CLOCKS_PER_SEC) * 1000;
+    printf("Running time for Bubble Sort  is %f ms\n", cpu_time_used);
+
+    // Insertion Sort
+    start = clock();
+    insertionSort(arr, size);
+    end = clock();
+    cpu_time_used = (((double) (end - start)) / CLOCKS_PER_SEC) * 1000;
+    printf("Running time for Insertion Sort  is %f ms\n", cpu_time_used);
+
+    // Selection Sort
+    start = clock();
+    selectionSort(arr, size);
+    end = clock();
+    cpu_time_used = (((double) (end - start)) / CLOCKS_PER_SEC) * 1000;
+    printf("Running time for Selection Sort  is %f ms\n", cpu_time_used);
+
+}
+
+
 int main() {
     // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
     // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    srand(time(nullptr));
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    int testSizes[] = {1000, 10000, 25000, 50000, 100000};
+
+    for (int size : testSizes) {
+        cout << "Testing size: " << size << endl;
+        measureRuntime(size);
+        cout << endl;
     }
 
     return 0;
