614fd22aa6b15f75e5a5f5e36d4b14e8ce211cc2
seifmaazouz <154283002+seifmaazouz@users.noreply.github.com>
2025-02-28 16:04:55 +0200

    Add test code

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-
+#include <vector>
 
 using namespace std;
 
-void bubbleSort(int arr[], int size);
-int* generateRandArray(int size);
+// void bubbleSort(int arr[], int size);
+// void insertionSort(int arr[], int size);
+// void selectionSort(int arr[], int size);
+
+// int* generateRandArray(int size);
 void measureRuntime(int size);
 
 int main() {
@@ -24,7 +27,7 @@ int main() {
     return 0;
 }
 
-void bubbleSort(int arr[], int size) {
+void bubbleSort1(int arr[], int size) {
     bool isSorted;
     for (int i = 0; i < size - 1; i++) {
         isSorted = true;
@@ -41,7 +44,31 @@ void bubbleSort(int arr[], int size) {
     }
 }
 
-int* generateRandArray(int size) {
+void insertionSort1(int arr[], int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void selectionSort1(int arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        swap(arr[i], arr[min]);
+    }
+}
+
+int* generateRandArray1(int size) {
     int* arr = new int[size];
 
     for (int i = 0; i < size; i++)
@@ -50,20 +77,111 @@ int* generateRandArray(int size) {
     return arr;
 }
 
+vector <int> generateRandArray(int size) {
+    vector <int> arr(size);
+    for (int i = 0; i < size; i++)
+        arr[i] = rand();
+
+    return arr;
+}
+
+void bubbleSort(vector<int> arr, int size) {
+    bool isSorted;
+    for (int i = 0; i < size - 1; i++) {
+        isSorted = true;
+        for (int j = 0; j < size - 1 - i; j++) {
+            // if value before is greater than after --> swap
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                isSorted = false;
+            }
+        }
+        if (isSorted) break;
+    }
+}
+
+
+
+void insertionSort(vector <int> arr, int size) {
+    for (int i = 1; i < size; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void selectionSort(vector <int> arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        swap(arr[i], arr[min]);
+    }
+}
+
 void measureRuntime(int size) {
     clock_t start, end;
     double cpu_time_used;
 
-    int* arr = generateRandArray(size);
+    int* arr = generateRandArray1(size);
+    int* arrCopy = new int[size];
+
+    vector<int> arr2 = generateRandArray(size);
 
     // Bubble Sort
+    // without copying taken into time measurement
+    memcpy(arrCopy, arr, size * sizeof(int));
     start = clock();
-    bubbleSort(arr, size);
+    bubbleSort1(arrCopy, size);
     end = clock();
     cpu_time_used = (((double) (end - start)) / CLOCKS_PER_SEC) * 1000;
-    printf("Running time for Bubble Sort  is %f ms\n", cpu_time_used);
+    printf("Running time for Bubble Sort without copying is %f ms\n", cpu_time_used);
+    // with copying taken into time measurement
+    start = clock();
+    bubbleSort(arr2, size);
+    end = clock();
+    cpu_time_used = (((double) (end - start)) / CLOCKS_PER_SEC) * 1000;
+    printf("Running time for Bubble Sort with copying is %f ms\n", cpu_time_used);
 
-    // Test other sorting algorithms...
+    // Insertion Sort
+    // without copying taken into time measurement
+    memcpy(arrCopy, arr, size * sizeof(int));
+    start = clock();
+    insertionSort1(arrCopy, size);
+    end = clock();
+    cpu_time_used = (((double) (end - start)) / CLOCKS_PER_SEC) * 1000;
+    printf("Running time for Insertion Sort without copying is %f ms\n", cpu_time_used);
+    // with copying taken into time measurement
+    start = clock();
+    insertionSort(arr2, size);
+    end = clock();
+    cpu_time_used = (((double) (end - start)) / CLOCKS_PER_SEC) * 1000;
+    printf("Running time for Insertion Sort with copying is %f ms\n", cpu_time_used);
+
+    // Selection Sort
+    // without copying taken into time measurement
+    memcpy(arrCopy, arr, size * sizeof(int));
+    start = clock();
+    selectionSort1(arrCopy, size);
+    end = clock();
+    cpu_time_used = (((double) (end - start)) / CLOCKS_PER_SEC) * 1000;
+    printf("Running time for Selection Sort without copying is %f ms\n", cpu_time_used);
+    // with copying taken into time measurement
+    start = clock();
+    selectionSort(arr2, size);
+    end = clock();
+    cpu_time_used = (((double) (end - start)) / CLOCKS_PER_SEC) * 1000;
+    printf("Running time for Selection Sort with copying is %f ms\n", cpu_time_used);
 
     delete[] arr;
+    delete[] arrCopy;
 }
